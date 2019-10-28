53d32a90d8a6fb815d908aad12b21380e3dd8774
saddamSalah <engsadam510@gmail.com>
2019-10-28 18:21:33 +0200

    Add files via upload

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+df = pd.read_csv('../Data/7282_1.csv')
+print(df.head())
+print(df.shape)
+df = df.loc[df['categories'] == 'Hotels']
+print(df.shape)
+df.to_csv('../Data/hotels.csv')
